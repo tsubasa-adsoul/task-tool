a1020ac5f88ecb377f80660f04115f122094f0ce
tsubasa-adsoul <tsubasa.adsoul@gmail.com>
2025-10-18 16:13:11 +0900

    Update socket.js

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,24 +1,10 @@
-import { io } from 'socket.io-client';
-
-// WebSocket接続を作成
-const socket = io('https://asana-backend-7vdy.onrender.com', {
-  transports: ['websocket', 'polling'],
-  reconnection: true,
-  reconnectionDelay: 1000,
-  reconnectionAttempts: 5
-});
-
-// 接続イベント
-socket.on('connect', () => {
-  console.log('WebSocket connected:', socket.id);
-});
-
-socket.on('disconnect', () => {
-  console.log('WebSocket disconnected');
-});
-
-socket.on('connect_error', (error) => {
-  console.error('WebSocket connection error:', error);
-});
+// WebSocket機能は一時的に無効化
+const socket = {
+  on: () => {},
+  emit: () => {},
+  off: () => {},
+  connect: () => {},
+  disconnect: () => {}
+};
 
 export default socket;
